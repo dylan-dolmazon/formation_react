fd57d99b811c75a3587a27084cb2e11afc0f5a77
DOLMAZON DYLAN p2001757 <dylan.dolmazon@etu.univ-lyon1.fr>
2022-04-26 15:05:41 +0200

    gestion du hook d'effet

--- src/App/component/Button/Button.js
+++ src/App/component/Button/Button.js
@@ -1,16 +1,27 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import style from './Button.module.css'
 import PropTypes from 'prop-types';
 
 const Button=(props)=>{
      const [isClicked, setClicked] = useState(false)
+     useEffect(() => {
+        console.log("update du state",isClicked);
+        if(isClicked){
+            setTimeout(()=>{
+              setClicked(false);  
+            },1000);
+        }
+    //    return () => {
+    //      effect
+    //    };
+     }, [isClicked])
     return (
     <button 
         className={style.Button} 
         style={ {color:props.color, backgroundColor:props.bgColor}}
         onClick={(evt)=>{
             // eslint-disable-next-line no-const-assign
-            setClicked(!isClicked);
+            setClicked(true);
             console.log("value de isClicked",isClicked);
         }}
         >
