2eb151eb82a0a92905ae2fe909cbb8b0a67a7a77
DOLMAZON DYLAN p2001757 <dylan.dolmazon@etu.univ-lyon1.fr>
2022-04-26 14:27:25 +0200

    add prop-type

--- src/App/component/Button/Button.js
+++ src/App/component/Button/Button.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import style from './Button.module.css'
+import PropTypes from 'prop-types';
 
 const Button=(props)=>{
     console.log(props);
@@ -10,5 +11,8 @@ const Button=(props)=>{
         Dylan
     </button>)
 }
-
+Button.propTypes={
+    bgColor: PropTypes.string.isRequired,
+    color: PropTypes.string.isRequired,
+}
 export default Button;
