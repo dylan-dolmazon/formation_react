2852db661f0961d987cc852c40ea6e5f52fd37ec
DOLMAZON DYLAN p2001757 <dylan.dolmazon@etu.univ-lyon1.fr>
2022-04-26 16:45:45 +0200

    component update

--- src/App/App.js
+++ src/App/App.js
@@ -3,13 +3,30 @@ import Button from './component/Button/Button';
 import './App.css'
 
 class App extends React.Component{
-  compteur=0;
+  constructor(props){
+    super(props);
+    this.state = {
+      compteur:0,
+      uneData:"Demat breizh"
+    };
+
+  }
+  componentDidMount(){
+    console.log("le composant est monté et pret a servir",arguments);
+  }
+  componentDidUpdate(prevProps,prevState){
+    console.log(prevProps,this.state);
+    console.log("%c%s","font-size:24pt;color:red;font-weight:900","--PREV----------------------NEW--");
+    console.log(prevState,this.state);
+  }
   render(){
     return (
       <div className="App" data-testid="App">
-        <div>Valeur du compteur : {this.compteur}</div>
+        <div>Valeur du compteur : {this.state.compteur}</div>
 
-        <Button bgColor="skyblue" onButtonClick={()=>{alert('le bouton est click'+ ': ' + this.compteur)}}>+1</Button>
+        <Button bgColor="skyblue" onButtonClick={()=>{
+          this.setState({compteur:this.state.compteur+1})
+        }}>+1</Button>
         <Button bgColor="tomato">-1</Button>
 
       </div>
