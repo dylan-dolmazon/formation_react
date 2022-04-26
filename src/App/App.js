dde2950620a937a77e087c75a1ac36a9ac619920
DOLMAZON DYLAN p2001757 <dylan.dolmazon@etu.univ-lyon1.fr>
2022-04-26 12:59:33 +0200

    ajout component button

---
+++ src/App/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import Button from './component/Button/Button';
+
+function App() {
+  return (
+    <div>
+      <Button/>
+      <Button/>
+      <Button/>
+      <Button/>
+      <Button/>
+    </div>
+  );
+}
+
+export default App;
