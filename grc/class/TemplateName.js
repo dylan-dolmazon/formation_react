52f740060c1e3867bb8e12edebc8bb1236f97ceb
DOLMAZON DYLAN p2001757 <dylan.dolmazon@etu.univ-lyon1.fr>
2022-04-27 09:56:31 +0200

    ajout TemplateName.js

---
+++ grc/class/TemplateName.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import style from './TemplateName.module.scss'
+
+const TemplateNameInitialState={}
+
+class TemplateName extends Component {
+    constructor(props){
+        super(props)
+        this.state = TemplateNameInitialState
+    }
+    render() {
+        return (
+            <div className={style.TemplateName} data-testid="templateName">
+                templateName
+            </div>
+        );
+    }
+}
+
+TemplateName.propTypes = {
+}
+
+TemplateName.defaultProps={
+}
+
+export default TemplateName
