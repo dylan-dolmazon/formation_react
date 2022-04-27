4627fcdcbb47a273a01d58faf48fc054e5931598
DOLMAZON DYLAN p2001757 <dylan.dolmazon@etu.univ-lyon1.fr>
2022-04-27 11:25:48 +0200

    cinfig meme

--- src/App/App.js
+++ src/App/App.js
@@ -1,8 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import FlexH from './layout/FlexH/FlexH'
 import FlexW from './layout/FlexW/FlexW'
 
+const appInitialState={meme:{
+  text:'Le react c est cool',
+  x:20,
+  y:20,
+  fonSize:20,
+  fontWeight:'900',
+  color:'#FFFF',
+  imageId:0,
+  name:'mon 1ier meme'
+
+  },
+  images:[
+    {
+      id:0,
+      name:'image',
+      url:'',
+      w:1280,
+      h:720
+    }
+  ]
+}
+
 function App() {
+  const [state, setstate] = useState(appInitialState)
   return (
     <div className='App' style={{height:'95vh'}}>
       <div>header</div>
