e47ff592eaee8b7d7219b3fe919fa544c3915e6d
DOLMAZON DYLAN p2001757 <dylan.dolmazon@etu.univ-lyon1.fr>
2022-04-27 17:04:19 +0200

    add server

--- src/App/App.js
+++ src/App/App.js
@@ -1,36 +1,34 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import MemeForm from './component/MemeForm/MemeForm'
 import MemeSvgViewer from './component/ui/MemeSvgViewer/MemeSvgViewer'
 import Navbar from './component/ui/Navbar/Navbar'
 import FlexH from './layout/FlexH/FlexH'
 import FlexW from './layout/FlexW/FlexW'
 
-const appInitialState={meme:{
-  text:'Le react c est cool',
-  x:20,
-  y:20,
-  fonSize:20,
-  fontWeight:'900',
-  color:'#FFFF',
-  imageId:0,
-  underline:true,
-  italic:false,
-  name:'mon 1ier meme'
-
+const appInitialState={
+  meme:{
+    text:'Le react c est cool',
+    x:20,
+    y:20,
+    fonSize:20,
+    fontWeight:'900',
+    color:'#FFFF',
+    imageId:0,
+    underline:true,
+    italic:false,
+    name:'mon 1ier meme'
   },
-  image:[
-    {
-      id:0,
-      name:'image',
-      url:'kb9.jpg',
-      w:710,
-      h:444
-    }
-  ]
+  image:[],
+  memes:[],
 }
 
 function App() {
   const [state, setstate] = useState(appInitialState)
+  useEffect(() => {
+    return () => {
+      fetch('https://localhost:5679/')
+    };
+  }, [])
   return (
     <div className='App' style={{height:'95vh'}}> 
       <div>header</div>
