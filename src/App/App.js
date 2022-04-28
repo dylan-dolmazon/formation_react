4270ae3844febbe18a6d0ee2156212920dbb2a5a
DOLMAZON DYLAN p2001757 <dylan.dolmazon@etu.univ-lyon1.fr>
2022-04-28 16:54:12 +0200

    lien autour thumbnail + http editable /id

--- src/App/App.js
+++ src/App/App.js
@@ -1,11 +1,13 @@
-import React from "react";
-import { BrowserRouter as Router, Route, NavLink, Routes, Link } from 'react-router-dom'
+import React, { useEffect } from "react";
+import { connect } from "react-redux";
+import { BrowserRouter as Router, Route, NavLink, Routes, Link, useLocation, useNavigate,useParams } from 'react-router-dom'
 import FlexH from "./components/layout/FlexH/FlexH";
 import FlexW from "./components/layout/FlexW/FlexW";
 import {ConnectedMemeForm} from "./components/MemeForm/MemeForm";
 import Thumbnail from "./components/Thumbnail/Thumbnail";
 import MemeSvgViewer from "./components/ui/MemeSvgViewer/ConnectedMemeSvgViewer";
 import Navbar from "./components/ui/Navbar/Navbar";
+import { ACTIONS_CURRENT } from "./store/store";
 
 function App(props) {
   return (
@@ -21,6 +23,10 @@ function App(props) {
               path="/editor" 
               element={<MemeEditor/>} 
             />
+            <Route 
+              path="/editor/:id" 
+              element={<MemeEditor/>} 
+            />
           </Routes>
         </FlexH>
         <div>Footer</div>
@@ -29,13 +35,33 @@ function App(props) {
   );
 }
 
-function MemeEditor(){
+const mstp=(s) => {
+  console.warn(s)
+  return {memes:s.ressources.memes}
+}
+
+const mdtp=(dispatch)=>{
+  return {
+    setCurrent:(meme)=>{
+      dispatch({type:ACTIONS_CURRENT.UPDATE_CURRENT,value:meme})
+    }
+  }
+}
+
+const MemeEditor =connect(mstp,mdtp) ((props) => {
+  const id=useParams().id
+  useEffect(() => {
+      props.setCurrent(props.memes.find( m => m.id === Number(id)))
+  
+  }, [props,id])
   return (
     <FlexW>
       <MemeSvgViewer/>
       <ConnectedMemeForm/>
     </FlexW>
   )
-}
+})
+
+//const MemeEditor=connect(mstp,mdtp)(UnConnectedMemeEditor)
 
 export default App;
--- src/App/components/Thumbnail/Thumbnail.js
+++ src/App/components/Thumbnail/Thumbnail.js
@@ -1,18 +1,23 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import { Link } from 'react-router-dom'
 import MemeSvgViewer from '../ui/MemeSvgViewer/MemeSvgViewer'
 import style from './Thumbnail.module.scss'
 export const Thumbnail = (props) => {
   return (
-    <div className={style.Thumbnail} test-id="Thumbnail">
-      {props.memes.map((e,i)=> 
-        <MemeSvgViewer 
-          key={`th${i}`}
-          meme={e} 
-          image ={props.images.find(img=>img.id===e.imageId)}
-          />
-        )}
-    </div>
+      
+          <div className={style.Thumbnail} test-id="Thumbnail">
+            {props.memes.map((e,i)=> 
+              <Link to={`/editor/${e.id}`} key={`th${i}`}>
+                <MemeSvgViewer 
+               
+                key={`th${i}`}
+                meme={e} 
+                image ={props.images.find(img=>img.id===e.imageId)}
+                />
+                </Link>
+              )}
+          </div>
   )
 }
 
