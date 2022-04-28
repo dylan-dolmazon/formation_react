2eb7dc67a3fd17c00cb7920446cad84146a9387e
DOLMAZON DYLAN p2001757 <dylan.dolmazon@etu.univ-lyon1.fr>
2022-04-28 14:47:59 +0200

    del import

--- src/App/App.js
+++ src/App/App.js
@@ -5,26 +5,6 @@ import {ConnectedMemeForm} from "./components/MemeForm/MemeForm";
 import Thumbnail from "./components/Thumbnail/Thumbnail";
 import MemeSvgViewer from "./components/ui/MemeSvgViewer/ConnectedMemeSvgViewer";
 import Navbar from "./components/ui/Navbar/Navbar";
-import { REST_ADR, REST_RESSOURCES } from "./config/config";
-import store from './store/store'
-
-
-const appInitialState = {
-  meme: {
-    text: "Le react c'est cool",
-    x: 20,
-    y: 20,
-    fontSize: 20,
-    fontWeight: "900",
-    color: "#000000",
-    imageId: 0,
-    underline: true,
-    italic: false,
-    name: "mon 1er meme",
-  },
-  images: [],
-  memes: [],
-};
 
 function App(props) {
   return (
